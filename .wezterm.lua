fe1105a005098dc0910f20340d1a313389b0a3e0
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2024-05-22 13:56:35 -0400

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'Blue Matrix'
+config.color_scheme = 'Abernathy'
+-- config.color_scheme = 'Argonaut'
+-- config.color_scheme = 'Bright (base16)'
+-- config.color_scheme = 'carbonfox'
+-- config.color_scheme = 'Cyberdyne'
+-- config.color_scheme = 'cyberpunk'
+-- config.color_scheme = 'Dark Pastel'
+-- config.color_scheme = 'Dark Violet (base16)'
+-- config.color_scheme = 'Dissonance (Gogh)'
+
+config.font = wezterm.font('JetBrains Mono')
+
+--config.window_background_opacity = 0.90
+
+-- and finally, return the configuration to wezterm
+return config
