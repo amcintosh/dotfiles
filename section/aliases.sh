7917679a93e92f8891c60fc932a818132ed1146b
Andrew McIntosh <andrew_mcintosh@intuit.com>
2025-01-08 16:54:38 -0500

    Fix editors, style

--- section/aliases.sh
+++ section/aliases.sh
@@ -1,9 +1,20 @@
-#My usual 'ls' alias
-alias ls='ls --color=auto'
-alias lsl='ls -lh'
+# My usual 'ls' alias
+if [[ "$OS_NAME" = "Darwin" ]]; then
+    alias ls='ls -G'
+    alias lsl='ls -lGho'
+else
+    alias ls='ls --color=auto'
+    alias lsl='ls --color=auto -lho'
+fi
+
 alias df='df -h'
 
 #Some helpful stuff
 alias more='less'
 alias ping='ping -c 5'
 alias hist="history | grep $1"
+
+if [[ "$OS_NAME" =  "Darwin" ]]; then
+    # OSX quicklook
+    alias look="qlmanage -p"
+fi
---
+++ section/appearance.sh
@@ -0,0 +1,6 @@
+# ls colours
+export CLICOLOR=1
+export LSCOLORS='Exfxcxdxbxegedabagacad'
+
+# ZSH Completion
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=248"
