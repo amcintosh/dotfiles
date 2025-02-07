66adce014a20db4758bf0695251264e0c9a22377
Andrew McIntosh <andrew_mcintosh@intuit.com>
2025-02-07 14:18:22 -0500

    Update intuit config

--- section/intuit.sh
+++ section/intuit.sh
@@ -6,13 +6,17 @@ then
     PATH="${M1_HOME}/bin:${PATH}"
     export PATH
 
+    # Rancher Desktop
+    export PATH="$HOME/.rd/bin:$PATH"
+
     # TTO Tomcat
     export CATALINA_HOME=/usr/local/intuit_apache-tomcat
-    alias stoptomcat='$CATALINA_HOME/bin/catalina.sh stop'
     alias starttomcat='$CATALINA_HOME/bin/catalina.sh start'
     alias cdtomcat='cd $CATALINA_HOME'
 
     # A few tools
+    alias what="$HOME/work/tools/what/node_modules/.bin/what-cli"
     alias kanban="$HOME/Personal/kanban/bin/kanban"
     alias notion="$HOME/Personal/notion/node_modules/.bin/notion-cli"
+
 fi
