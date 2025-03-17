ee1fa101d57c56a4cf3be8f7f7c5aa0b084396da
Andrew McIntosh <andrew_mcintosh@intuit.com>
2025-03-17 10:25:42 -0400

    Update docker aliases

--- section/docker.sh
+++ section/docker.sh
@@ -3,6 +3,8 @@ if [[ -d "$HOME/.rd/bin" ]]; then
     export PATH="$HOME/.rd/bin:$PATH"
 fi
 
-alias wipe-all-containers='docker rm -f `docker ps -qa`'
-alias wipe-all-images='docker rmi `docker images -q`'
+alias docker-stop-all='docker stop `docker ps -q`'
+alias docker-kill-all='docker kill `docker ps -q`'
+alias docker-wipe-all-containers='docker rm -f `docker ps -qa`'
+alias docker-wipe-all-images='docker rmi `docker images -q`'
 alias docker-free-space='docker rmi `docker images --quiet --filter "dangling=true"`'
